b0f428aad027e757bc08c29cba3d501bf57cab53
Yenhj <yen.greact@gmail.com>
2023-04-10 13:11:17 +0900

    탭메뉴 생성중

--- js/script.js
+++ js/script.js
@@ -36,5 +36,62 @@ $(document).ready(function () {
   });
 
   //   mWin.fadeIn(300);
-  mWin.show();
+  mWin.hide();
+
+  // 탭메뉴
+  let tabStart = 2; // 자동 초기 포커스
+  const tabWrap = $(".tabWrap");
+  const tabMenuBts = $(".tabMenu a");
+  const tabBoxs = $(".tabBox");
+  const tabSpeed = 3000;
+  let tabTimer = setInterval(changeTabMenu, tabSpeed);
+
+  // 타이머를 멈춘다.(탭 컨텐츠에 마우스 오버)
+  tabWrap.mouseenter(function () {
+    clearInterval(tabTimer);
+  });
+  // 타이머를 재시작.(탭 컨텐츠에 마우스 아웃)
+  tabWrap.mouseleave(function () {
+    clearInterval(tabTimer);
+    tabTimer = setInterval(changeTabMenu, tabSpeed);
+  });
+
+  function changeTabMenu() {
+    // 일단 탭내용들은 모두 class 는 제거
+    tabBoxs.removeClass("tabFocus");
+    // index 에 해당하는 탭내용만 class 추가
+    tabBoxs.eq(tabStart).addClass("tabFocus");
+    // 일단 모든 탭메뉴의 class 는 제거
+    tabMenuBts.removeClass("tabFocus");
+    // 클릭된 a 태그만 class 추가
+    tabMenuBts.eq(tabStart).addClass("tabFocus");
+    // tabStart 증가
+    tabStart = tabStart + 1;
+    if (tabStart > 3) {
+      tabStart = 0;
+    }
+    console.log(tabStart);
+  }
+
+  changeTabMenu();
+  // 클릭 기능
+  // tabMenuBts[0].click();
+  // tabMenuBts[1].click();
+  // tabMenuBts[2].click();
+  // tabMenuBts[3].click();
+
+  // tabMenuBts.forEach(function(item, index) {})
+  $.each(tabMenuBts, function (index, item) {
+    $(item).click(function (e) {
+      // go class 를 가지고 있니?
+      const go = $(this).hasClass("go");
+      if (go) return;
+
+      // a 태그 href 막기
+      e.preventDefault();
+
+      tabStart = index;
+      changeTabMenu();
+    });
+  });
 });
